5060491fac3cff157042782a65de53bc33de5919
Crisroman <lu.n.ponce@hotmail.com>
2017-09-13 15:28:18 -0300

    Segundo commit
    Le añadi el enunciado

--- EjercicioUno.java
+++ EjercicioUno.java
@@ -13,8 +13,11 @@ public class EjercicioUno {
 
     /**
      * @param args the command line arguments
+     * Se debe mostrar el siguiente mensaje:
+     * Esto funciona de maravilla
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
     }
     
